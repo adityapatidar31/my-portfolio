e95fb6494da1a768dd1a4efd4ed2f1d110f154a1
Aditya Patidar <adityapatidar.2020@gmail.com>
2024-12-13 22:58:52 +0530

    add my name in the Hero Component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,18 @@
-function Hero() {
-  return <div>Hero</div>;
-}
-
+const Hero = () => {
+  return (
+    <section className="bg-emerald-100 py-24  ">
+      <div className=" mx-auto max-w-7xl  px-8  grid  md:grid-cols-2 items-center gap-8">
+        <article>
+          <h1 className="text-7xl font-bold tracking-wider">I&apos;m Aditya</h1>
+          <p className="mt-4 text-3xl text-slate-700 capitalize tracking-wide">
+            Fullstack Web Developer
+          </p>
+          <p className="mt-2 text-lg text-slate-700 capitalize tracking-wide">
+            turning ideas into interactive reality
+          </p>
+        </article>
+      </div>
+    </section>
+  );
+};
 export default Hero;
