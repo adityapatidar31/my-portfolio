169b40b0aebd440fb66f0ae200db5ff5bb66878d
Aditya Patidar <adityapatidar.2020@gmail.com>
2024-12-14 00:10:42 +0530

    complete the sectionTitle Component

--- src/components/SectionTitle.jsx
+++ src/components/SectionTitle.jsx
@@ -1,5 +1,9 @@
-function SectionTitle() {
-  return <div>SectionTitle SectionTitle</div>;
-}
-
+/* eslint-disable react/prop-types */
+const SectionTitle = ({ text }) => {
+  return (
+    <div className="border-b border-gray-200 pb-5">
+      <h2 className="text-3xl font-medium tracking-wider capitalize">{text}</h2>
+    </div>
+  );
+};
 export default SectionTitle;
